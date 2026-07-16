68a91ca154969c626732bf5d7588210c6f6aa7d5
Yunseong <dev.yunseong@gmail.com>
2026-07-16 11:20:55 +0900

    feat: 백그라운드 키 입력 테스트 추가

---
+++ Assets/Scenes/Test/KeyboardTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public sealed class KeyboardTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Return key was released.");
+        }
+    }
+}
